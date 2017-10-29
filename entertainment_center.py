a2fea3f7c608479648ebde46e4ba8970e079492b
Ayush Didwaniya <didwaniya.ayush@gmail.com>
2017-10-30 01:30:40 +0530

    python comipled files

--- entertainment_center.py
+++ entertainment_center.py
@@ -1,9 +1,12 @@
-import fresh_tomatoes                                        # import fresh_tomatoes to access the function open_movies_page()!!
-import media                                                 # to access the class Movie!!
+# import fresh_tomatoes to access the function open_movies_page()!!
+import fresh_tomatoes
+# import media to access the class Movie!!
+import media
+#sending attributes value in function __init__ of class Movie for instance padmavati!!
 padmavati = media.Movie("padmavati",            # we are creating each instances for each movie by movie name!!       
                         "a story of queen padmini",
                         "https://upload.wikimedia.org/wikipedia/en/4/47/Padmavati_Poster.jpg",
-                        "https://www.youtube.com/watch?v=X_5_BLt76c0")          #sending attributes value in function __init__ of class Movie for instance padmavati!!
+                        "https://www.youtube.com/watch?v=X_5_BLt76c0")
 toystory = media.Movie("toy story",
                        "a story of and is toys that comes to life",
                        "https://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg",
@@ -13,7 +16,7 @@ avatar = media.Movie("avatar",
                      "https://upload.wikimedia.org/wikipedia/en/b/b0/Avatar-Teaser-Poster.jpg",
                      "https://www.youtube.com/watch?v=cRdxXPV9GNQ")
 minions = media.Movie("minions",
-                      "Minions hilariously imagines centuries in which the little guys have sought to serve the greatest villain they could find, but quickly settles into more conventional cartoon territory.",
+                      "Minions hilariously imagines centuries in which the search for greatest villain they could find",
                       "https://upload.wikimedia.org/wikipedia/en/3/3d/Minions_poster.jpg",
                       "www.youtube.com/watch?v=vATjQSarl8g")
 midnight_in_paris = media.Movie("midnight in paris",
@@ -28,10 +31,14 @@ hunger_games = media.Movie("hunger games",
                            "A really real reality show",
                            "https://upload.wikimedia.org/wikipedia/en/4/42/HungerGamesPoster.jpg",
                            "https://www.youtube.com/watch?v=4S9a5V9ODuY")
+
 movies = [padmavati,toystory, avatar, minions, midnight_in_paris, ratatouille, hunger_games]    #list of moives intances !!
 fresh_tomatoes.open_movies_page(movies)
+
 """
     The file fresh_tomatoes.py contains the open_movies_page() function that will take in your list of
     movies and generate an HTML file including this content, producing a website to showcase your favorite movies
+
+
+    print(media.Movie.__doc__)            // this will print the documentation of Movie class which is in media module !!
 """
-# print(media.Movie.__doc__)            // this will print the documentation of Movie class which is in media module !!
